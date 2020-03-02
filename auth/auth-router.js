727eb165c68684a6eb1a98a05658760216c49643
Eian Carter <eian@bricleir.com>
2020-03-02 10:23:43 -0800

    added registration route

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -4,3 +4,18 @@ const jwt = require("jsonwebtoken");
 
 const Users = require("../users/users-model");
 const { jwtSecret } = require("../config/secrets");
+
+router.post("/register", (req, res) => {
+    let user = req.body;
+    // hash #: 8 for development only
+    const hash = bcrypt.hashSync(user.password, 8);
+    user.password = hash;
+
+    Users.add(user)
+        .then(saved => {
+            res.status(201).json(saved);
+        })
+        .catch(error => {
+            res.status(500).json(error);
+        });
+});
