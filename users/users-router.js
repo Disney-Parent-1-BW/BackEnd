10b743bc066b7a5018572918181707530cf924b9
Eian Carter <eian@bricleir.com>
2020-03-02 10:20:25 -0800

    full crud for users router complete

--- users/users-router.js
+++ users/users-router.js
@@ -12,4 +12,56 @@ router.get("/", (req, res) => {
         })
 });
 
+router('/:id', (req, res) => {
+    const { id } = req.params;
+
+    Users.findById(id)
+    .then(user => {
+        if(user) {
+            res.json(user);
+        } else {
+            res.status(404).json({ message: 'could not retrieve that user' })
+        }
+    })
+    .catch(err => {
+        res.status(500).json({ message: 'failed to retrieve user'})
+    })
+})
+
+router.put('/:id', (req, res) => {
+    const { id } = req.params;
+    const changes = req.body;
+
+    Users.findById(id)
+    .then(user => {
+        if(user) {
+            Users.update(changes, id)
+            .then(updatedUser => {
+                res.json(updatedUser);
+            });
+        } else {
+            res.status(404).json({ message: 'could not find that user'})
+        }
+    })
+    .catch(err => {
+        res.status(500).json({ message: 'failed to update user' })
+    });
+});
+
+router.delete('/:id', (req, res) => {
+    const { id } = req.params;
+
+    Users.remove(id)
+    .then(deleted => {
+        if(deleted) {
+            res.json({ removed: deleted });
+        } else {
+            res.status(404).json({ message: 'cold not delete that user' })
+        }
+    })
+    .catch(err => {
+        res.status(500).json({ message: 'failed to delete users'})
+    });
+});
+
 module.exports = router;
