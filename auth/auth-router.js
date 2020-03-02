17f3b94b1942012a2c8428e474cac110fb446c0a
Joshua Agins <56888002+jagins@users.noreply.github.com>
2020-03-02 15:39:48 -0600

    users works with passing token changed Authorization to authorization

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -12,6 +12,11 @@ router.post("/register", (req, res) => {
 
     Users.add(user)
         .then(saved => {
+            if(saved.isProviders == 0) {
+                saved.isProviders = true
+            } else {
+                saved.isProviders = false   
+            }
             res.status(201).json(saved);
         })
         .catch(error => {
