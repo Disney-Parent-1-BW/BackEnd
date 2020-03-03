c4e9ef8e2ef0958c76eca8ca3c3ac16181bed8a1
Eian Carter <eian@bricleir.com>
2020-03-03 09:38:23 -0800

    tables complete

--- data/migrations/20200303091049_requests_accreq_userratings_table.js
+++ data/migrations/20200303091049_requests_accreq_userratings_table.js
@@ -34,9 +34,35 @@ exports.up = function(knex) {
             .onDelete('RESTRICT')
         table.unique(['request_id', 'accepted_by']);
     })
+    .createTable('user_ratings', table => {
+        table.increments();
+        table
+            .integer('rating_left_by')
+            .unsigned()
+            .notNullable()
+            .references('id')
+            .inTable('users')
+            .onUpdate('CASCADE')
+            .onDelete('RESTRICT')
+        table
+            .string('message', 256)
+        table
+            .integer('rating_for')
+            .unsigned()
+            .notNullable()
+            .references('id')
+            .inTable('users')
+            .onUpdate('CASCADE')
+            .onDelete('RESTRICT')
+        table
+            .integer('rating')
+        table.unique(['rating_left_by', 'rating_for']);
+    })
 };
 
 exports.down = function(knex) {
   return knex.schema
-    .dropTableIfExists('users');
+    .dropTableIfExists('requests')
+    .dropTableIfExists('accepted_requests')
+    .dropTableIfExists('user_ratings')
 };
