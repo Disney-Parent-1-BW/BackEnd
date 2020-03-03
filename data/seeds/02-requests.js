f6bb9f4405379200226c4ea9f2125e636d0ed809
Eian Carter <eian@bricleir.com>
2020-03-03 10:44:06 -0800

    updated requests seed

--- data/seeds/02-requests.js
+++ data/seeds/02-requests.js
@@ -1,13 +1,13 @@
 
 exports.seed = function(knex) {
   // Deletes ALL existing entries
-  return knex('table_name').del()
+  return knex('requests').del()
     .then(function () {
       // Inserts seed entries
-      return knex('table_name').insert([
-        {id: 1, colName: 'rowValue1'},
-        {id: 2, colName: 'rowValue2'},
-        {id: 3, colName: 'rowValue3'}
+      return knex('requests').insert([
+        {id: 1, location: 'USA', time:'2000-12-31 20:30:00', requestor_id: '1' },
+        {id: 1, location: 'USA', time:'2000-01-15 18:30:00', requestor_id: '2' },
+        {id: 1, location: 'USA', time:'2000-01-11 12:00:00', requestor_id: '3' },
       ]);
     });
 };
