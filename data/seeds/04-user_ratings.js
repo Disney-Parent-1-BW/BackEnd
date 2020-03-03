5bd5da34360728941c60ebd7cc819dd325b0ccb7
Eian Carter <eian@bricleir.com>
2020-03-03 10:54:13 -0800

    updated user ratings seed

--- data/seeds/04-user_ratings.js
+++ data/seeds/04-user_ratings.js
@@ -1,13 +1,13 @@
 
 exports.seed = function(knex) {
   // Deletes ALL existing entries
-  return knex('table_name').del()
+  return knex('user_ratings').del()
     .then(function () {
       // Inserts seed entries
-      return knex('table_name').insert([
-        {id: 1, colName: 'rowValue1'},
-        {id: 2, colName: 'rowValue2'},
-        {id: 3, colName: 'rowValue3'}
+      return knex('user_ratings').insert([
+        {id: 1, rating_left_by: 1, message: 'they did a great job!', rating_for: 2, rating: 5},
+        {id: 2, rating_left_by: 2, message: 'my child was returned unharmed!', rating_for: 1, rating: 5},
+        {id: 3, rating_left_by: 3, message: 'they lost my kid!', rating_for: 2, rating: 1},
       ]);
     });
 };
