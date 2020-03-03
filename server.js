434175da9d532207a5a290fb462440d3228dc3ba
Joshua Agins <56888002+jagins@users.noreply.github.com>
2020-03-03 17:48:31 -0600

    planning out messages router getting whole conversation will have to be be plugged into accepted requests

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const authRouter = require("./auth/auth-router");
 const usersRouter = require("./users/users-router");
 const restricted = require("./auth/restricted-middleware");
 const kidsRouter = require('./kids/kids-router');
-
+const messagesRouter = require('./messages/messagesRouter');
 
 const server = express();
 
@@ -17,6 +17,7 @@ server.use(cors());
 server.use("/api/auth", authRouter);
 server.use('/api/users/kids', restricted, kidsRouter)
 server.use("/api/users", restricted, usersRouter);
+server.use('/api/acceptedRequests/messsages', messagesRouter)
 
 server.get("/", (req, res) => {
     res.send(`<h2>Welcome to Disney!</h2>`);
