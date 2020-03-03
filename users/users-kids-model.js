083a11b611b908abdeae43a1066aad0c6452a8c4
Joshua Agins <56888002+jagins@users.noreply.github.com>
2020-03-03 12:29:42 -0600

    user-kids-model

---
+++ users/users-kids-model.js
@@ -0,0 +1,22 @@
+const db = require('../data/dbConfig');
+
+function find()
+{
+
+}
+
+function add()
+{
+
+}
+
+function findById()
+{
+
+}
+
+module.exports = {
+    find,
+    add,
+    findById
+}
