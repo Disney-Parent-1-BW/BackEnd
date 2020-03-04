968c40774bca2cb44713dd4f8e7cdb9d7fac99cb
Joshua Agins <56888002+jagins@users.noreply.github.com>
2020-03-03 18:00:17 -0600

    skeleton for message db helpers

---
+++ messages/messages-model.js
@@ -0,0 +1,28 @@
+const db = require('../data/dbConfig');
+
+function getMessages()
+{
+
+}
+
+function getMessageById()
+{
+
+}
+
+function updateMessage()
+{
+
+}
+
+function deleteMessage()
+{
+
+}
+
+module.exports = {
+    getMessages,
+    getMessageById,
+    updateMessage,
+    deleteMessage
+}
