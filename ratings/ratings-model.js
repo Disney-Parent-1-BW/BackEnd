31ff28ed112ff54382ddda4a6f21dece9da33987
Eian Carter <eian@bricleir.com>
2020-03-03 19:30:12 -0800

    ratings router complete with crud tested

--- ratings/ratings-model.js
+++ ratings/ratings-model.js
@@ -2,12 +2,31 @@ const db = require("../data/dbConfig");
 
 module.exports = {
     find,
-    // findById,
-    // add,
-    // update,
-    // remove
+    findById,
+    add,
+    remove
 }
 
 function find() {
     return db("user_ratings");
+}
+
+function findById(id) {
+    return db("user_ratings")
+    .where({ id })
+}
+
+function add(ratingData) {
+    return db("user_ratings")
+    .insert(ratingData)
+    .then(ids => {
+      db("user_ratings")
+        .where({ id: ids[0] })
+    })
+  }
+
+function remove(id) {
+    return db("user_ratings")
+    .where("id", id)
+    .del()
 }
--- ratings/ratings-router.js
+++ ratings/ratings-router.js
@@ -13,4 +13,51 @@ router.get("/", (req, res) => {
     })
 })
 
+//getting a rating by id
+router.get("/:id", (req, res) => {
+    const { id } = req.params;
+
+    Ratings.findById(id)
+    .then(rating => {
+        if(rating) {
+            res.json(rating);
+        } else {
+            res.status(404).json({ message: 'Could not find rating with that id' })
+        }
+    })
+    .catch(err => {
+        res.status(500).json({ message: 'Failed to get request'})
+    })
+});
+
+//posting a new rating
+router.post("/", (req, res) => {
+    const ratingData = req.body;
+    Ratings.add(ratingData)
+    .then(rating => {
+        res.status(201).json(rating)
+    })
+    .catch(err => {
+    console.log("post rating error", err);
+    res.status(500).json({ message: "Failed to post rating data" });
+    });
+  });
+
+//removing a rating
+router.delete("/:id", (req, res) => {
+    const { id } = req.params;
+
+    Ratings.remove(id)
+    .then(deleted => {
+        if(deleted) {
+            res.json({ removed: deleted })
+        } else {
+            res.status(404).json({ message: 'Could not find rating with that id' })
+        }
+    })
+    .catch(err => {
+        res.status(500).json({ message: 'Failed to delete rating' })
+    })
+})
+
 module.exports = router;
