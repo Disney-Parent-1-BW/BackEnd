91977ffa597b1f91031f9b2cf47aec324918716a
Eian Carter <eian@bricleir.com>
2020-03-04 12:40:10 -0800

    removed duplicate requests endpoint in server

--- server.js
+++ server.js
@@ -19,13 +19,9 @@ server.use(cors());
 server.use("/api/auth", authRouter);
 server.use('/api/users/kids', restricted, kidsRouter)
 server.use("/api/users", restricted, usersRouter);
-<<<<<<< HEAD
 server.use('/api/acceptedRequests', restricted, messagesRouter)
 server.use("/api/requests", restricted, requestRouter);
-=======
-server.use("/api/requests", requestRouter);
 server.use("/api/ratings", ratingsRouter);
->>>>>>> 4e17030d1a1cbb7884a94f3259198000a934a32e
 
 server.get("/", (req, res) => {
     res.send(`<h2>Welcome to Disney!</h2>`);
