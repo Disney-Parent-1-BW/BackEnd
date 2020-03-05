aeed42f4b36a79e84d2830241175bc7fa388d9c1
Joshua Agins <56888002+jagins@users.noreply.github.com>
2020-03-04 22:08:25 -0600

    wip

--- data/migrations/20200303091049_requests_accreq_userratings_table.js
+++ data/migrations/20200303091049_requests_accreq_userratings_table.js
@@ -62,7 +62,7 @@ exports.up = function(knex) {
 
 exports.down = function(knex) {
   return knex.schema
-    .dropTableIfExists('requests')
-    .dropTableIfExists('accepted_requests')
     .dropTableIfExists('user_ratings')
+    .dropTableIfExists('accepted_requests')
+    .dropTableIfExists('requests')
 };
