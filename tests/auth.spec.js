e358cd2f2cf62eed806fb2eecb852ead5314f712
Joshua Agins <56888002+jagins@users.noreply.github.com>
2020-03-05 19:38:56 -0600

    login test pass

--- tests/auth.spec.js
+++ tests/auth.spec.js
@@ -2,9 +2,9 @@ const request = require('supertest');
 
 const server = require('../server');
 
-describe('POST /api/auth/register', function ()
+describe('POST /api/auth/register', function()
 {
-    test('should return 201 created', function ()
+    test('should return 201 created', function()
     {
         const user = {
             name: "Edward Scissorhands",
@@ -18,6 +18,26 @@ describe('POST /api/auth/register', function ()
             .then(res =>
             {
                 expect(res.status).toBe(201);
+            })
+    })
+
+})
+
+describe('POST /api/auth/login', function()
+{
+    test('user can log in', function()
+    {
+        const user = {
+            username: "scissors123",
+            password: "sweenyTodd"
+        };
+
+        return request(server)
+            .post('/api/auth/login')
+            .send(user)
+            .then(res =>
+            {
+                expect(typeof res.body.token).toBe('string');
             })
     })
 })
